bddf40b292552b6adb1cbbe2ea8165100d6f96ef
Dhanashri-Borage <67874520+Dhanashri-Borage@users.noreply.github.com>
2020-07-09 08:07:37 +0530

    Update script.js

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -16,7 +16,7 @@ Good Bye Jim
 */
 
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yaadnya", "Jack", "Jayashri", "Janson", "Pallavi", "Faisu", "Lata", "Pratik", "Laura", "Jimmy"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
